57fcdeaa29346ed0cd0b1690eaf40b966eb3edcd
mishadoff <mishadoff@gmail.com>
2012-03-30 16:45:11 +0300

    Initial commit

---
+++ src/nlputils/segment.clj
@@ -0,0 +1,28 @@
+(ns nlputils.segment)
+
+;; http://www.mieliestronk.com/wordlist.html
+(def DICT (into #{} (re-seq #"[\w']+" (.toLowerCase (slurp "res/words.txt")))))
+
+(defn segment-min [string dict]
+  "Perform string segmentation by dictionary with minimum matching algorithm."
+  (loop [s string idx 1 res []]
+    (cond (empty? s) res
+          (< (count s) idx) []
+          :else (let [curstr (subs s 0 idx)]
+                  (if (contains? dict curstr)
+                    (recur (subs s idx) 1 (conj res curstr))
+                    (recur s (inc idx) res))))))
+
+(defn segment-max [string dict]
+  "Perform string segmentation by dictionary with maximum matching algorithm."
+  (loop [s string idx (count s) res []]
+    (cond (empty? s) res
+          (< idx 0) []
+          :else (let [curstr (subs s 0 idx)]
+                  (if (contains? dict curstr)
+                    (recur (subs s idx) (- (count s) idx) (conj res curstr))
+                    (recur s (dec idx) res))))))
+
+(defn segment-bruteforce [string dict]
+  ;; TODO implement
+  )
