0824cd47935bc49fa9c9636d2212ddd9a3d1d0a7
mishadoff <mishadoff@gmail.com>
2012-03-31 22:16:03 +0300

    ML algorithms implemented

--- src/nlputils/segment.clj
+++ src/nlputils/segment.clj
@@ -2,6 +2,35 @@
 
 ;; http://www.mieliestronk.com/wordlist.html
 (def DICT (into #{} (re-seq #"[\w']+" (.toLowerCase (slurp "res/words.txt")))))
+;; file with human audit results
+(def GOLD (map #(re-seq #"[\d\w]+" %) (seq (.split (.toLowerCase (slurp "res/twitter-hashtags.txt")) "\n"))))
+(def TRAIN-WORDS (re-seq #"[\d\w]+" (.toLowerCase (slurp "res/big.txt"))))
+
+(defn- train-unigram [words]
+  (frequencies TRAIN-WORDS))
+
+(defn- train-bigram [words]
+  (frequencies (partition 2 1 words)))
+
+;; models
+;; TODO make n gram model
+(def UNIGRAM-MODEL (train-unigram TRAIN-WORDS))
+(def UNIGRAM-SIZE (reduce + (vals UNIGRAM-MODEL)))
+(def UNIGRAM-VOC-SIZE (count (vals UNIGRAM-MODEL)))
+(def BIGRAM-MODEL (train-bigram TRAIN-WORDS))
+(def BIGRAM-SIZE (reduce + (vals BIGRAM-MODEL)))
+(def BIGRAM-VOC-SIZE (count (vals BIGRAM-MODEL)))
+
+(defn- log-probability-unigram [words]
+  (reduce + (map #(Math/log (double (/ (inc (get UNIGRAM-MODEL % 0))
+                                       (+ UNIGRAM-SIZE
+                                          UNIGRAM-VOC-SIZE)))) words)))
+
+(defn- log-probability-bigram [words]
+  (if (= 1 (count words)) (log-probability-unigram words)
+      (reduce + (map #(Math/log (double (/ (inc (get BIGRAM-MODEL % 0))
+                                           (+ BIGRAM-SIZE
+                                              BIGRAM-VOC-SIZE)))) (partition 2 1 words)))))
 
 (defn segment-min [string dict]
   "Perform string segmentation by dictionary with minimum matching algorithm."
@@ -38,11 +67,27 @@
 
 (defn segment-bruteforce [string dict]
   "Perform string segmentation by dictionary with bruteforce"
-  (filter #(contains-all? % dict) (word-candidates-all string dict)))
+  (let [cands (filter #(contains-all? % dict) (word-candidates-all string dict))
+        segm (first (first (reverse (sort-by second (map #(list % (log-probability-unigram %)) cands)))))]
+    segm))
 
-(defn segment-bruteforce-clever [string dict]
+(defn- word-candidates-dict [string dict]
   (if (empty? string) [""]
       (apply concat (for [i (range 1 (inc (count string)))
                           :let [ss (subs string 0 i)]
                           :when (contains? dict ss)]
-                      (map #(cons ss %) (segment-bruteforce-clever (subs string i) dict))))))
+                      (map #(cons ss %) (word-candidates-dict (subs string i) dict))))))
+
+(defn segment-bruteforce-clever [string dict]
+  "Perform string segmentation by dictionary with clever bruteforce"
+  (let [cands (word-candidates-dict string dict)
+    segm (first (first (reverse (sort-by second (map #(list % (log-probability-bigram %)) cands)))))]
+    segm))
+
+(defn calculate-accuracy [segf dict gold-set]
+  (loop [[g & gs] gold-set correct 0]
+    (if g
+      (if (= (segf (first g) dict) (rest g))
+        (recur gs (inc correct))
+        (recur gs correct))
+      (double (/ correct (count gold-set))))))
