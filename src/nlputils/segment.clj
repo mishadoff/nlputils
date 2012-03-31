3b44578cee32e68f91490961c8015d01c4ea3641
mishadoff <mishadoff@gmail.com>
2012-03-31 15:29:47 +0300

    Bruteforce segmentation algorithm added

--- src/nlputils/segment.clj
+++ src/nlputils/segment.clj
@@ -23,6 +23,22 @@
                     (recur (subs s idx) (- (count s) idx) (conj res curstr))
                     (recur s (dec idx) res))))))
 
+(defn- split-string-at [string idx]
+  [(subs string 0 idx)
+   (subs string idx)])
+
+(defn- contains-all? [words dict]
+  (reduce #(and %1 %2) (map #(contains? dict %) words)))
+
+(defn- word-candidates-all [string dict]
+  (if (empty? string) [""]
+      (apply concat (for [i (range 1 (inc (count string)))]
+                      (let [[a b] (split-string-at string i)]
+                        (map #(cons a %) (word-candidates-all b dict)))))))
+
 (defn segment-bruteforce [string dict]
-  ;; TODO implement
+  "Perform string segmentation by dictionary with bruteforce"
+  (filter #(contains-all? % dict) (word-candidates-all string dict)))
+
+(defn segment-clever-bruteforce [string dict]
   )
