ce1653dd9e77648fbd31e5cd0f9761e5992a9edf
mishadoff <mishadoff@gmail.com>
2012-04-20 14:41:05 +0300

    Soundex extended algorithm added

--- src/nlputils/speech.clj
+++ src/nlputils/speech.clj
@@ -8,21 +8,45 @@
                     \m 5 \n 5
                     \r 6})
 
+;; extended soundex codes
+(def SOUNDEX_EXT_CODES {\b 1 \p 1
+                    \f 2 \v 2
+                    \c 3 \k 3 \s 3
+                    \g 4 \j 4
+                    \q 5 \x 5 \z 5
+                    \d 6 \t 6
+                    \l 7
+                    \m 8 \n 8
+                    \r 9})
+
 ;; letters ignored by soundex code
 (def SOUNDEX_IGNORE_1 #{\a \e \i \o \u \y})
 (def SOUNDEX_IGNORE_2 #{\h \w})
 
 (defn soundex [name]
-  "Get soundex-code for given name. Only English"
+  "Get soundex-code for given name using codes. Only English.
+   Soundex code has always fixed 4-chars-length code (e.g M576)"
   (let [[a & b] (.toLowerCase name)]
     (.toUpperCase (subs (str (apply str a
-           (remove #(= -1 %)
-                   (loop [[l & ls] (map #(get SOUNDEX_CODES % -1) (remove #(contains? SOUNDEX_IGNORE_2 %) b))
-                          prev (get SOUNDEX_CODES a -1) ls-new []]
+           (remove #(= 0 %)
+                   (loop [[l & ls] (map #(get SOUNDEX_CODES % 0) (remove #(contains? SOUNDEX_IGNORE_2 %) b))
+                          prev (get SOUNDEX_CODES a 0) ls-new []]
                      (if l
                        (if (= l prev)
                          (recur ls prev ls-new)
                          (recur ls l (conj ls-new l))) ls-new)))) "000") 0 4))))
+
+(defn soundex-extended [name]
+  "Get extended soundex-code for given name using codes. Only English.
+   Extended soundex has non-fixed size of code."
+  (let [[a & b] (.toLowerCase name)]
+    (.toUpperCase (apply str a
+         (loop [[l & ls] (map #(get SOUNDEX_EXT_CODES % 0) (remove #(contains? SOUNDEX_IGNORE_2 %) (.toLowerCase name)))
+                prev nil ls-new []]
+           (if l
+             (if (= l prev)
+               (recur ls l ls-new)
+               (recur ls l (conj ls-new l))) ls-new))))))
 
 (defn similar? [name1 name2 strategy]
   "Check if two names are similar using strategy.
